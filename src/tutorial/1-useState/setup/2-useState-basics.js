bcc159602e802fd0b1e312313b102689ecea989c
NBinte <nuzhatbintei@gmail.com>
2021-08-31 14:47:04 +0600

    Toggle text using state

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  // console.log(useState("Boop"));
 
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+
+  // console.log(value, handler);
+
+  const [text, setText] = useState("meh");
+
+  const clickHandler = () => {
+    if (text === "meh") {
+      setText("Boop");
+    } else {
+      setText("meh");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={clickHandler}>
+        change title
+      </button>
+    </React.Fragment>
+  );
+};
 export default UseStateBasics;
