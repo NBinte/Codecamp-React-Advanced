f29482759bc04bfbdeb79a7804ccd8f4ba20ef85
NBinte <nuzhatbintei@gmail.com>
2021-09-01 10:56:47 +0600

    Create a simple counter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 //import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 // import Setup from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/1-useState/setup/4-useState-object";
+//import Setup from "./tutorial/1-useState/setup/4-useState-object";
+import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 function App() {
   return (
     <div className="container">
