2ce48902be761d72dc10f4b1d2dd1209e9144e5c
NBinte <nuzhatbintei@gmail.com>
2021-09-01 12:46:22 +0600

    Use functional update

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,6 +7,15 @@ const UseStateCounter = () => {
     setValue(0);
   };
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
@@ -20,6 +29,15 @@ const UseStateCounter = () => {
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           Increase
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More complex counter</h2>
+        <h1>{value}</h1>
+
+        <button className="btn" onClick={complexIncrease}>
+          Increase later
         </button>
       </section>
     </>
