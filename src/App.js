ddd5a0243b6f74ec52c7a5260bc34e76af449e52
NBinte <nuzhatbintei@gmail.com>
2021-09-01 13:05:45 +0600

    Refactor using functional update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React from "react";
 //import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 // import Setup from "./tutorial/1-useState/setup/3-useState-array";
 //import Setup from "./tutorial/1-useState/setup/4-useState-object";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+// import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+import Final from "./tutorial/1-useState/final/3-useState-array";
 function App() {
   return (
     <div className="container">
-      <Setup />
+      <Final />
     </div>
   );
 }
--- src/tutorial/1-useState/final/3-useState-array.js
+++ src/tutorial/1-useState/final/3-useState-array.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    setPeople((oldPeople) => {
+      let newPeople = oldPeople.filter((person) => person.id !== id);
+
+      return newPeople;
+    });
   };
   return (
     <>
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button className='btn' onClick={() => setPeople([])}>
+      <button className="btn" onClick={() => setPeople([])}>
         clear items
       </button>
     </>
