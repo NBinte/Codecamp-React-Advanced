926bff228f2d5a338c579036405530220c608220
NBinte <nuzhatbintei@gmail.com>
2021-09-05 11:48:45 +0600

    Render based on short circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Boba tea");
+  //const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>Value: {secondValue}</h1> */}
+      {/* {
+    if(){
+      console.log("This will throw an error");
+    }
+
+  } */}
+
+      <h1>{text || "Milk tea"}</h1>
+
+      {/* {!text && <h1>Want Some?</h1>} */}
+
+      {text && <h1>Want Some?</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
