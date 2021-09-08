3a0b7c22563599563727f81595cc5bc3362ed671
NBinte <nuzhatbintei@gmail.com>
2021-09-08 10:11:12 +0600

    Handle input with useRef

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import React from "react";
 // import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 // import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 //import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
-import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
+// import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
+import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
 
 function App() {
   return (
--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,38 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+
+  const divContainer = useRef(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+
+  console.log(refContainer);
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+  return (
+    <>
+      <form action="" className="form" onSubmit={submitHandler}>
+        <div>
+          <input type="text" ref={refContainer} />
+
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>Boba Tea</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
