ceeb7224671d19af45bfc7001e1776eaf139193a
NBinte <nuzhatbintei@gmail.com>
2021-09-08 17:55:28 +0600

    Create an example of prop drilling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ import React from "react";
 //import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 // import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 // import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
-import Setup from "./tutorial/6-useReducer/setup";
+// import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 
 function App() {
   return (
