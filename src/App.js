31ab1120178e6254c6a83cfdb5ea20b8fc8c5ec8
NBinte <nuzhatbintei@gmail.com>
2021-09-09 14:58:53 +0600

    Create a custom hook for fetching data

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import React from "react";
 // import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
 // import Setup from "./tutorial/6-useReducer/setup";
 // import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
+// import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 
 function App() {
   return (
