c513f66f232985eea86702149586d1bc1c805407
NBinte <nuzhatbintei@gmail.com>
2021-09-12 16:34:58 +0600

    Add memo function

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ import React from "react";
 // import Setup from "./tutorial/8-useContext/setup/1-context-api";
 // import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 // import Setup from "./tutorial/10-prop-types/setup";
-import Setup from "./tutorial/11-react-router/setup";
+// import Setup from "./tutorial/11-react-router/setup";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 
 function App() {
   return (
