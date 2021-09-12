f6d87ff45cd7034013da968ac205155560fed451
NBinte <nuzhatbintei@gmail.com>
2021-09-12 11:17:25 +0600

    Setup react router

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Person from "./Person";
 // navbar
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/people">
+        <People />
+      </Route>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
