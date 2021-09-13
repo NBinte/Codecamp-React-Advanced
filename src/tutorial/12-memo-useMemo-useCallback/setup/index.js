45021ee8ca99e97a77460655a572452f5f03c6fc
NBinte <nuzhatbintei@gmail.com>
2021-09-13 10:45:46 +0600

    Add useCallback function

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useMemo } from "react";
-import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
+import { useFetch } from "../../9-custom-hooks/setup/2-useFetch";
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
@@ -11,31 +11,46 @@ const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
 
+  const [cart, setCart] = useState(0);
+
+  console.log(cart);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
+
   return (
     <>
       <h1>Count : {count}</h1>
       <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem" }}>Cart: {cart}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   );
 };
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   useEffect(() => {
     console.log("big list called");
   });
   return (
     <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>;
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
   );
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(() => {
     console.count("single product called");
   });
@@ -48,6 +63,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>Add to cart</button>
     </article>
   );
 };
